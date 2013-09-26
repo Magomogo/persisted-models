2acf8f24fd1e4c6b2ed049d56db9abce942a96b4
Magomogo <mgnatenko@gmail.com>
2013-09-26 11:18:24 +0700

    natural keys enabled

--- lib/Magomogo/Persisted/Container/CouchDb.php
+++ lib/Magomogo/Persisted/Container/CouchDb.php
@@ -63,6 +63,9 @@ class CouchDb implements ContainerInterface
             $doc = array_merge_recursive($doc, $existingDoc);
             $this->client->putDocument($doc, $properties->id($this));
         } else {
+            if (!is_null($properties->naturalKey())) {
+                $doc['_id'] = $properties->naturalKey();
+            }
             list($id, $rev) = $this->client->postDocument($doc);
             $properties->persisted($id, $this);
         }
