d11107eb45e67bb2c6424fdcee828d2aef33d5f1
Magomogo <mgnatenko@gmail.com>
2016-09-10 16:12:09 +0200

    update dependencies, fix tests

--- test/Persisted/Container/SqlDbTest.php
+++ test/Persisted/Container/SqlDbTest.php
@@ -3,9 +3,9 @@ namespace Magomogo\Persisted\Container;
 
 use Mockery as m;
 use Magomogo\Persisted\Test\DbNames;
-use Magomogo\Persisted\Test\ObjectMother\Person as TestPerson;
 use Magomogo\Persisted\AbstractProperties;
 use Magomogo\Persisted\Test\Person;
+use Hamcrest\Matchers;
 
 class SqlDbTest extends \PHPUnit_Framework_TestCase
 {
@@ -17,8 +17,8 @@ class SqlDbTest extends \PHPUnit_Framework_TestCase
     public function testFollowsTableNamingConvention()
     {
         $db = self::dbMock();
-        $db->shouldReceive('insert')->with('person', typeOf('array'))->once();
-        $db->shouldReceive('insert')->with('creditcard', typeOf('array'))->once();
+        $db->shouldReceive('insert')->with('person', Matchers::typeOf('array'))->once();
+        $db->shouldReceive('insert')->with('creditcard', Matchers::typeOf('array'))->once();
         $properties = new Person\Properties;
         $properties->putIn(self::container($db));
     }
